2e545c4061240501fea4ad3c67869618db410c83
Robinss <robinsswei@gmail.com>
2016-10-10 17:43:24 +0000

    init project

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Test file at: 
+ * https://docs.google.com/spreadsheets/d/1QQtmeZwY25O8M5Vb1Pg-DBedO-udD0VD9EDMweOBUwI/edit?usp=sharing
+ * 
+ * /
+
+/*global $*/
+$(function(){
+  
+  console.log("window is ready");
+});
